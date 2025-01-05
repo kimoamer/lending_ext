3a7d592a0b447240268d1f9ff99e8b2dc415d136
kimoamer <farisamer2020@gmail.com>
2025-01-05 23:23:17 +0200

    ok

--- lending_ext/hooks.py
+++ lending_ext/hooks.py
@@ -82,7 +82,7 @@ app_license = "mit"
 # Installation
 # ------------
 
-before_install = "lending_ext.patch.fix_Loan_Repayment_Schedule"
+before_install = "lending_ext.patch.fix_schedule,fix_Loan_Repayment_Schedule"
 # after_install = "lending_ext.install.after_install"
 
 # Uninstallation
